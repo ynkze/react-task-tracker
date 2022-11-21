998ee8e56c128f6966e0e11b9ae5e92853588780
yiheng.tey <yiheng.tey@shopee.com>
2022-11-21 18:23:44 +0800

    Finished task tracker with basic functionalities

---
+++ build/precache-manifest.57c83b89c4b2f293028793d0f701f292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ec786d3dfa3b4e5e861",
+    "url": "/static/css/main.298ab6e8.chunk.css"
+  },
+  {
+    "revision": "8ec786d3dfa3b4e5e861",
+    "url": "/static/js/main.d5b90e08.chunk.js"
+  },
+  {
+    "revision": "78e09dfe97033ba1a92a",
+    "url": "/static/js/runtime~main.bc900c8a.js"
+  },
+  {
+    "revision": "b71eee5765d760f31807",
+    "url": "/static/js/2.fcfd0f61.chunk.js"
+  },
+  {
+    "revision": "ce165983f250aaa94dda",
+    "url": "/static/js/3.49a205bf.chunk.js"
+  },
+  {
+    "revision": "78c4e07f8dab565147d6f8dc1e971aaa",
+    "url": "/index.html"
+  }
+];
